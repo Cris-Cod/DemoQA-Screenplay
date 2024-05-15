4ce2f0b158bca66f113ecd073d6b6087c89b1f05
Cristian <aarevalo.cristian.17@gmail.com>
2024-05-15 17:02:18 -0500

    changes in fill form

--- src/main/java/screenplay/user_interface/FormsLocators.java
+++ src/main/java/screenplay/user_interface/FormsLocators.java
@@ -17,7 +17,7 @@ public class FormsLocators {
     public static final Target INPUT_DATE_BIRTH  = Target.the("input date birth").located(By.id("dateOfBirthInput"));
     public static final Target SELECT_MONTH  = Target.the("select month").located(By.xpath("//select[@class='react-datepicker__month-select']"));
     public static final Target SELECT_YEAR  = Target.the("select year").located(By.xpath("//select[@class='react-datepicker__year-select']"));
-    public static final Target SELECT_DAY  = Target.the("select day").located(By.xpath("//div[@class='react-datepicker__day react-datepicker__day--'"+date+"']"));
+    public static final Target SELECT_DAY  = Target.the("select day").located(By.xpath("//div[@class='react-datepicker__day react-datepicker__day--023']"));
     public static final Target INPUT_SUBJECT  = Target.the("input subject").located(By.id("subjectsContainer"));
     public static final Target CHECKBOX  = Target.the("checkbox").located(By.id("hobbies-checkbox-1"));
     public static final Target UPLOADPICTURE = Target.the("upload picture").located(By.id("uploadPicture"));
